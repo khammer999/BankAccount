89d3e56edf3e1273119ad0f35b2d3c0faf264b0c
khammer999 <khammer999@gmail.com>
2018-09-25 15:41:09 -0400

    initial commit

---
+++ AccountTests/AccountTests.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestFixture]
+
+    public class AccountTests
+    {
+
+        /*// leave for later
+        [Test]
+        public void Test_Client_CTR()
+        {
+            Test_Client
+
+        }
+        */ // leave for later
+
+        [Test]
+        public void Test_LName_Prop()
+        {
+            //arrange
+            Client myClient = new Client();
+
+            //action
+
+            myClient.LName = "Doh";
+            string response = myClient.LName;
+
+            //assert
+
+            Assert.That(response, Is.EqualTo("Doh"));
+        }
+
+        [Test]
+        public void Test_FName_Prop()
+        {
+            //arrange
+            Client myClient = new Client();
+
+            //action
+
+            myClient.FName = "John";
+            string response = myClient.FName;
+
+            //assert
+
+            Assert.That(response, Is.EqualTo("John"));
+        }
+
+        [Test]
+        public void Test_AccountNum_Prop()
+        {
+            //arrange
+            Client myClient = new Client();
+
+            //action
+
+            myClient.AccountNum = "0101010";
+            string response = myClient.AccountNum;
+
+            //assert
+
+            Assert.That(response, Is.EqualTo("0101010"));
+        }
+
+        [Test]
+        public void Test_Client_Menu_CTR()
+        {
+            //arrange
+            Client myClient = new Client("John","Doh","0101010");
+
+            //action
+
+            
+            string response = myClient.GetConstString();
+
+            //assert
+
+            Assert.That(response, Is.EqualTo("Welcome " + myClient.FName + " " + myClient.LName + ". Your Acount number is " + myClient.AccountNum ));
+
+        }
+
+        [Test]
+        public void Test_Account_CTR()
+        {
+            //arrange
+            Account myAccount = new Account();
+
+            //action
+
+            myClient.LName = "Doh";
+            string response = myClient.LName;
+
+            //assert
+
+            Assert.That(response, Is.EqualTo("Doh"));
+        }
+
+
+
+    }
+}
---
+++ BankAccount/Client.cs
@@ -0,0 +1,57 @@
+﻿namespace BankAccount
+{
+    public class Client
+
+
+
+
+
+
+    {
+        //Fields
+        //Properties
+
+        public string LName { get; set; }
+        public string FName { get; set; }
+        public string AccountNum { get; set; }
+
+        //Constructors
+
+        public Client()
+        {
+            //defalt constructor
+        }
+
+
+        public Client(string first, string last, string accountNum)
+        {
+            //overload constructor 3 perameters
+
+            this.FName = first;
+            this.LName = last;
+            this.AccountNum = accountNum;
+
+        }
+
+        //Methods
+
+        public string GetConstString()
+        {            
+            string result= "Welcome " + FName +" " + LName + ". Your Acount number is "+ AccountNum ;
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
